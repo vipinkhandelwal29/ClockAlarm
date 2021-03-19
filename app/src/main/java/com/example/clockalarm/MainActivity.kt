a6cb31072dcf516c050b09a1c7831344d79bf4da
Vipin Khandelwal <vipink.spaceo@gmail.com>
2021-03-19 19:01:14 +0530

    Alarm with workermanager done start and stop

--- app/src/main/java/com/example/clockalarm/MainActivity.kt
+++ app/src/main/java/com/example/clockalarm/MainActivity.kt
@@ -1,111 +1,73 @@
 package com.example.clockalarm
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.app.TimePickerDialog
+import android.content.Context
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.work.OneTimeWorkRequest
-import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.clockalarm.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 
-class MainActivity : BaseActivity<ActivityMainBinding>(), AdapterView.OnItemSelectedListener {
-    private var interval: Long? = null
-    private var fromTime: Long? = null
-    private var toTime: Long? = null
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
 
     private lateinit var alarmManager: AlarmManager
     private lateinit var pendingIntent: PendingIntent
+
     override fun getLayoutId() = R.layout.activity_main
 
     override fun initControl() {
+        var interval: Long? = null
+        val fromTime: Calendar = Calendar.getInstance()
+        val toTime: Calendar = Calendar.getInstance()
 
-        var timePicker: TimePickerDialog
-        val currentTime = Calendar.getInstance()
-        val h = currentTime.get(Calendar.HOUR_OF_DAY)
-        val m = currentTime.get(Calendar.MINUTE)
-
-
-        val adapterBG = ArrayAdapter.createFromResource(
+        val adapter = ArrayAdapter.createFromResource(
             applicationContext,
             R.array.Timer,
             android.R.layout.simple_spinner_item
         )
-        adapterBG.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinner.adapter = adapterBG
-        binding.spinner.onItemSelectedListener = this
-
-
-        /* binding.tvTime1.setOnClickListener {
-             timePicker = TimePickerDialog(
-                 this,
-                 TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute->
-                     binding.tvTime1.text = String.format("%d : %d :%d", hourOfDay, minute)
-                 }, h, m, true
-             )
-             timePicker.show()
-         }
-
-         binding.tvTime2.setOnClickListener {
-
-             timePicker = TimePickerDialog(
-                 this,
-                 TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                     binding.tvTime2.text = String.format("%d : %d", hourOfDay, minute)
-                 }, h, m, true
-             )
-             timePicker.show()
-         }*/
-
-
-        binding.tvTime1.setOnClickListener {
-            timePicker = TimePickerDialog(
-                this,
-                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                    binding.tvTime1.text = String.format("%d : %d", hourOfDay, minute)
-                    currentTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
-                    currentTime.set(Calendar.MINUTE, minute)
-                    currentTime.set(Calendar.SECOND, 0)
-                    currentTime.set(Calendar.MILLISECOND, 0)
-                    fromTime = currentTime.timeInMillis
-                }, h, m, false
-            )
-            timePicker.show()
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = adapter
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                interval = parent!!.getItemAtPosition(position).toString().toLong()
+            }
         }
+        val pref = this.getSharedPreferences("interval",Context.MODE_PRIVATE)
 
-        binding.tvTime2.setOnClickListener {
-            timePicker = TimePickerDialog(
-                this,
-                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                    binding.tvTime2.text = String.format("%d : %d", hourOfDay, minute)
-                    currentTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
-                    currentTime.set(Calendar.MINUTE, minute)
-                    currentTime.set(Calendar.SECOND, 0)
-                    currentTime.set(Calendar.MILLISECOND, 0)
-                    toTime = currentTime.timeInMillis
-                }, h, m, false
-            )
-            timePicker.show()
-        }
+
+
+        setTime(binding.tvTime1, fromTime)
+        setTime(binding.tvTime2, toTime)
 
         binding.btnSetAlarm.setOnClickListener {
-            val time = interval!! * 60 * 1000
 
-            val myPeriodicWorkRequest =
-                PeriodicWorkRequestBuilder<NotifyWork>(time, TimeUnit.MILLISECONDS).build()
-            WorkManager.getInstance(this).enqueue(myPeriodicWorkRequest)
-
-            /*val notificationWork = OneTimeWorkRequest.Builder(NotifyWork::class.java)
-                .setInitialDelay(time, TimeUnit.MILLISECONDS).build()
-            Log.d("==>ne", "$time,$notificationWork")
-
-            WorkManager.getInstance(this).enqueue(notificationWork)*/
+            /*
+                        val oneTimeWorkRequest =
+                            PeriodicWorkRequestBuilder<NotifyWork>(time, TimeUnit.MILLISECONDS).build()
+                        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+                            "Vipin",
+                            ExistingPeriodicWorkPolicy.KEEP,
+                            myPeriodicWorkRequest)
+                        */
+            val editor = pref.edit().putLong("intervalData",binding.spinner.selectedItem.toString().toLong()).commit()
+            val editor1=  pref.edit().putLong("endTime", toTime.timeInMillis).commit()
+            val notificationWork = OneTimeWorkRequest.Builder(NotifyWork::class.java).addTag("startTag")
+                .setInitialDelay(
+                    (fromTime.timeInMillis - System.currentTimeMillis()), TimeUnit.MILLISECONDS).build()
+            Log.d("==>",  "$notificationWork")
+            WorkManager.getInstance(this).enqueue(notificationWork)
 
             /*  alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
               val intent = Intent(this, MyBroadCastReceiver::class.java)
@@ -116,10 +78,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), AdapterView.OnItemSele
         }
     }
 
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        interval = parent!!.getItemAtPosition(position).toString().toLong()
+    @SuppressLint("SimpleDateFormat")
+    private fun setTime(textView: TextView, calendar: Calendar ) {
+        textView.setOnClickListener {
+            TimePickerDialog(this,
+                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                    calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                    calendar.set(Calendar.MINUTE, minute)
+                    calendar.set(Calendar.SECOND, 0)
+                    calendar.set(Calendar.MILLISECOND, 0)
+                    textView.text =
+                        SimpleDateFormat("hh:mm aa").format(Date(calendar.timeInMillis))
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/example/clockalarm/NotifyWork.kt
+++ app/src/main/java/com/example/clockalarm/NotifyWork.kt
@@ -1,13 +1,37 @@
 package com.example.clockalarm
 
 import android.content.Context
-import androidx.work.Worker
-import androidx.work.WorkerParameters
+import android.util.Log
+import android.widget.Toast
+import androidx.work.*
 import com.example.clockalarm.util.getNotification
+import java.sql.Time
+import java.util.concurrent.TimeUnit
 
 class NotifyWork(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
+
         applicationContext.getNotification("Reminder", "Please get Up")
+
+        val interval = applicationContext.getSharedPreferences("interval", Context.MODE_PRIVATE)
+        val intervalData =interval.getLong("intervalData",0)
+        val endTime =interval.getLong("endTime",0)
+        val eHour = TimeUnit.MILLISECONDS.toHours(endTime)
+        val eMin = TimeUnit.MILLISECONDS.toMinutes(endTime)
+
+        Log.d("==>me", "$intervalData,$endTime")
+
+        if (TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis()) == eHour && TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis()) == eMin)
+        {
+            WorkManager.getInstance(applicationContext).cancelAllWorkByTag("startTag")
+            Log.d("==>", "Break")
+        }else
+        {
+            val notificationWork = OneTimeWorkRequest.Builder(NotifyWork::class.java)
+                .setInitialDelay(intervalData * 60 * 1000, TimeUnit.MILLISECONDS).build()
+            WorkManager.getInstance(applicationContext).enqueue(notificationWork)
+        }
+
         return Result.success()
     }
 }
